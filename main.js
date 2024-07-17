ecf1b4d183086dea666045302fc1803122db7efc
nagisora <nagisora.icanspeng@gmail.com>
2024-07-17 22:25:06 +0900

    リファクタリング

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ import './style.css';
 const onClickAdd = () => {
   const inputText = document.getElementById('add-text').value;
   document.getElementById('add-text').value = '';
+  creatIncompleteTodo(inputText);
+};
 
+const creatIncompleteTodo = (todo) => {
   const li = document.createElement('li');
 
   const div = document.createElement('div');
@@ -11,7 +14,7 @@ const onClickAdd = () => {
 
   const p = document.createElement('p');
   p.className = 'todo-item';
-  p.innerText = inputText;
+  p.innerText = todo;
 
   const completeButton = document.createElement('button');
   completeButton.innerText = '完了';
@@ -39,6 +42,7 @@ const onClickAdd = () => {
   li.appendChild(div);
 
   document.getElementById('incomplete-list').appendChild(li);
+
 };
 
 document.getElementById('add-button').addEventListener('click', onClickAdd);
