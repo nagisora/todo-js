6448a4856cf2748f48e38d496892ec06da93c69f
nagisora <52764918+nagisora@users.noreply.github.com>
2024-07-17 22:02:54 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+import './style.css';
+
+const onClickAdd = () => {
+  const inputText = document.getElementById('add-text').value;
+  document.getElementById('add-text').value = '';
+
+  const li = document.createElement('li');
+
+  const div = document.createElement('div');
+  div.className = 'list-row';
+
+  const p = document.createElement('p');
+  p.className = 'todo-item';
+  p.innerText = inputText;
+
+  const completeButton = document.createElement('button');
+  completeButton.innerText = '完了';
+  completeButton.addEventListener('click', () => {
+    const moveTarget = deleteButton.closest('li');
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+
+    const backButton = document.createElement('button');
+    backButton.innerText = '戻す';
+    moveTarget.firstElementChild.appendChild(backButton);
+    document.getElementById('complete-list').appendChild(moveTarget);
+  });
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '削除';
+  deleteButton.addEventListener('click', () => {
+    const deleteTarget = deleteButton.closest('li');
+    document.getElementById('incomplete-list').removeChild(deleteTarget);
+  });
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  document.getElementById('incomplete-list').appendChild(li);
+};
+
+document.getElementById('add-button').addEventListener('click', onClickAdd);
